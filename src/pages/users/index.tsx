d82167eb90bf456a94f37550c01dd4e90127d591
Mesquini <mesquini@live.com>
2023-03-08 16:15:36 -0300

    add axios and react-query (create fake api)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Header";
 import { Pagination } from "@/components/Pagination";
 import { Siderbar } from "@/components/Sidebar";
+import { api } from "@/services/api";
 import {
   Box,
   Button,
@@ -8,6 +9,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -19,8 +21,32 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 
 export default function UserList() {
+  const { data, isLoading, error, isFetching } = useQuery(
+    "users",
+    async () => {
+      const { data } = await api.get("/users");
+
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
+
+      return users;
+    },
+    { staleTime: 1000 * 5 }
+  );
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -37,6 +63,9 @@ export default function UserList() {
           <Flex mb="8" justify={"space-between"} align="center">
             <Heading size="lg" fontWeight={"normal"}>
               Usuários
+              {isFetching && !isLoading && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href={"/users/create"} passHref>
@@ -52,46 +81,63 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme={"whiteAlpha"}>
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width={8}>
-                  <Checkbox colorScheme={"pink"} />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width={8}></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme={"pink"} />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight={"bold"}>Victor mesquni</Text>
-                    <Text fontSize={"sm"} color="gray.300">
-                      mesquini@live.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2014</Td>}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize={"sm"}
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify={"center"}>
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify={"center"}>
+              <Text>Falha ao obter os dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme={"whiteAlpha"}>
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width={8}>
+                      <Checkbox colorScheme={"pink"} />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width={8}></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme={"pink"} />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight={"bold"}>{user.name}</Text>
+                            <Text fontSize={"sm"} color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize={"sm"}
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                          >
+                            Editar
+                          </Button>
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,51 @@
+import {createServer, Factory, Model} from 'miragejs'
+import { faker } from '@faker-js/faker';
+
+type User = {
+  name: string
+  email: string
+  password: string
+  created_at: string
+}
+
+export function makeServer() {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    factories: {
+      user: Factory.extend({
+        name(i) {
+          return `User ${i+1}`
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        password() {
+          return faker.internet.password();
+        },
+        createdAt() {
+          return faker.date.recent(10)
+        },
+      })
+    },
+
+    seeds(server) {
+      server.createList('user', 10)
+    },
+
+    routes() {
+      this.namespace = 'api';
+      this.timing = 750;
+
+      this.get('/users');
+      this.post('/users');
+
+      this.namespace = '';
+      this.passthrough()
+    },
+  })
+
+  return server;
+}
